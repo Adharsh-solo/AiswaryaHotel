36b51116e4ed9b90f15987dd53e0868b8cadda6c
Adharsh-solo <adharshpb6@gmail.com>
2025-09-20 09:27:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import { BrowserRouter,Route,Routes } from "react-router-dom";
-import Menu from "./pages/Menu";
 import Navbar from "./components/Navbar";
 import Cart from "./pages/Cart";
 import { useState } from "react";
@@ -23,7 +22,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home onAddToCart={handleAddToCart}/>  }/>
         <Route path="/contact" element={<Contact />} />
-        <Route path="/menu" element={<Menu />} />
         <Route path="/about" element={<About />} />
         <Route path="/cart" element={<Cart cartItems={cartItems} onRemoveItem={(id) => {
     setCartItems(prev => prev.filter(item => item.id !== id));}} clearCart={clearCart}/>} />
